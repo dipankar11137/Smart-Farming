6a17977e7c105b6c3cd48fb7afc2546dcb3a4f15
dipankar-halder <dipankar15-11137@diu.edu.bd>
2023-11-25 23:21:09 +0600

    login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React from 'react';
 import {
   useSignInWithEmailAndPassword,
   useSignInWithGoogle,
-} from "react-firebase-hooks/auth";
-import { useForm } from "react-hook-form";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import auth from "../../firebase.init";
-// import axios from "axios";
-import Loading from "../Share/Loading";
-import login from "../../Images/Login/login.jpg";
-import { toast } from "react-toastify";
+} from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+
+import Loading from '../Share/Loading';
 
 const Login = () => {
-  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [gUser, gLoading, gError] = useSignInWithGoogle(auth);
   const {
     register,
     formState: { errors },
@@ -25,7 +23,7 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  let from = location.state?.from?.pathname || "/";
+  let from = location.state?.from?.pathname || '/';
 
   if (user || gUser) {
     navigate(from, { replace: true });
@@ -42,87 +40,96 @@ const Login = () => {
       </p>
     );
   }
-  const onSubmit = async (data) => {
-    const email = data.email;
+  const onSubmit = async data => {
     await signInWithEmailAndPassword(data.email, data.password);
-    toast.success("Successfully Login");
-    // const { accessToken } = await axios.post(
-    //   "https://boxberry.onrender.com/login",
-    //   {
-    //     email,
-    //   }
-    // );
-    // console.log(accessToken);
   };
 
   return (
-    <div className="flex justify-center h-screen bg-slate-700">
-      <div className="w-4/12 pt-40">
-        <img className="w-11/12 rounded-xl" src={login} alt="" />
-      </div>
-      <div className="flex justify-center items-center ">
-        <div className="card w-96 shadow-2xl bg-violet-50">
-          <div className="card-body">
-            <h2 className="text-center text-2xl">Login</h2>
+    <div
+      style={{
+        backgroundImage: `url("https://wallpapers.com/images/hd/dark-sunset-sky-m8irpdem6awbfkpk.jpg")`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100vp',
+      }}
+      className="flex justify-center h-screen bg-slate-700"
+    >
+      <div className="mt-32  ">
+        <div
+          style={{
+            backgroundImage: `url("https://c4.wallpaperflare.com/wallpaper/297/878/431/artstation-digital-art-building-night-moon-hd-wallpaper-preview.jpg")`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
+            boxShadow: '2px ',
+          }}
+          className="card w-96 shadow-2xl bg-violet-50"
+        >
+          <div className="card-body text-white">
+            <h2 className="text-center text-3xl font-bold">Login</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-control w-full max-w-xs">
+              <div className="form-control w-full ">
                 <label className="label">
-                  <span className="label-text">Email</span>
+                  <span className="label-text text-white">Email</span>
                 </label>
                 <input
+                  style={{ width: '400px' }}
                   type="email"
                   placeholder="Your Email"
-                  className="input input-bordered bg-white w-full max-w-xs"
-                  {...register("email", {
+                  className="input input-bordered bg-white text-black  w-96 "
+                  {...register('email', {
                     required: {
                       value: true,
-                      message: "Email is Required",
+                      message: 'Email is Required',
                     },
                     pattern: {
                       value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                      message: "Provide a valid Email",
+                      message: 'Provide a valid Email',
                     },
                   })}
                 />
                 <label className="label">
-                  {errors.email?.type === "required" && (
+                  {errors.email?.type === 'required' && (
                     <span className="label-text-alt text-red-500">
                       {errors.email.message}
                     </span>
                   )}
-                  {errors.email?.type === "pattern" && (
+                  {errors.email?.type === 'pattern' && (
                     <span className="label-text-alt text-red-500">
                       {errors.email.message}
                     </span>
                   )}
                 </label>
               </div>
-              <div className="form-control w-full max-w-xs">
+              <div className="form-control w-full ">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text text-white">Password</span>
                 </label>
                 <input
                   type="password"
                   placeholder="Password"
-                  className="input input-bordered bg-white w-full max-w-xs"
-                  {...register("password", {
+                  className="input input-bordered text-black font-bold bg-white w-full "
+                  {...register('password', {
                     required: {
                       value: true,
-                      message: "Password is Required",
+                      message: 'Password is Required',
                     },
                     minLength: {
                       value: 6,
-                      message: "Must be 6 characters or longer",
+                      message: 'Must be 6 characters or longer',
                     },
                   })}
                 />
                 <label className="label">
-                  {errors.password?.type === "required" && (
+                  {errors.password?.type === 'required' && (
                     <span className="label-text-alt text-red-500">
                       {errors.password.message}
                     </span>
                   )}
-                  {errors.password?.type === "minLength" && (
+                  {errors.password?.type === 'minLength' && (
                     <span className="label-text-alt text-red-500">
                       {errors.password.message}
                     </span>
@@ -132,26 +139,20 @@ const Login = () => {
 
               {signInError}
               <input
-                className="btn btn-orange-500 w-full text-white"
+                className="btn btn-primary w-full text-white"
                 type="submit"
                 value="Login"
               />
             </form>
-            <p>
-              <small>
-                New to BoxBerry Motor?{" "}
-                <Link to="/createAccount" className="text-orange-600 font-bold">
-                  Create New Account
-                </Link>
-              </small>
-            </p>
+            <p></p>
             <div className="divider">OR</div>
-            <button
-              onClick={() => signInWithGoogle()}
-              className="btn btn-outline font-black"
+
+            <Link
+              to="/createAccount"
+              className="btn btn-accent text-xl text-white font-bold  "
             >
-              Continue With Google
-            </button>
+              Create New Account
+            </Link>
           </div>
         </div>
       </div>
